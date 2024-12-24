a57981f55b453097ccb81c956b1193219b7b4785
Aza <aziko050050@gmail.com>
2024-12-25 02:04:16 +0600

    Ui commit

---
+++ src/main/java/HeadUi/FrontEnd.java
@@ -0,0 +1,11 @@
+package HeadUi;
+
+public class FrontEnd {
+    public static void main(String[] args) {
+        System.out.println("FrontEnd");
+        System.out.println("FrontEnd");
+        System.out.println("FrontEnd");
+        System.out.println("FrontEnd");
+        System.out.println("FrontEnd");
+    }
+}

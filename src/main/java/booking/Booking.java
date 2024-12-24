e760609a88471b6c5add61916bd08ef7c07e1cfc
Aza <aziko050050@gmail.com>
2024-12-24 23:52:37 +0600

    Changed packages

---
+++ src/main/java/booking/Booking.java
@@ -0,0 +1,7 @@
+package booking;
+
+public class Booking {
+    public static void main(String[] args) {
+        System.out.println("This is Booking class");
+    }
+}
---
+++ src/main/java/login/Password.java
@@ -0,0 +1,7 @@
+package login;
+
+public class Password {
+    public static void main(String[] args) {
+        System.out.println("Password class");
+    }
+}
